bd070f965abc4d10080af06e0e6c4c95f87d207e
Amit1332 <amitsinghpatel9747@gmail.om>
2023-07-31 22:59:59 +0530

    eight

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let singleCardTrip = document.querySelector("#singleCardTrip")
 let addNewItems = document.querySelector(".add_new_items")
 let blurPage = document.getElementsByClassName("blur-page")
 let noitems  =document.getElementById('noitems')
+let findcrationpopup =document.querySelector(".findcrationpopup")
+let findcrationpopuph1 =document.querySelector(".findcrationpopup h1")
+
 //  Add Card Popup
 var flag =0
 function addNewTaskPopup(){
@@ -44,7 +47,20 @@ var id=0
 
 // card creation
 function addTripCard(){
-   id++
+   findcrationpopup.setAttribute("id","creationpopup")
+   if(cardTitle.value==''){
+   findcrationpopup.style.backgroundColor = 'rgb(210, 149, 8)'
+   findcrationpopuph1.innerHTML= 'Please enter Place'
+
+      setTimeout(() => {
+   findcrationpopuph1.innerHTML= ''
+
+      findcrationpopup.removeAttribute("id")
+       
+      }, 1000);
+   }
+   else{
+      id++
    // create Element for card
    let card = document.createElement("div")
    let h2Text = document.createElement("h2")
@@ -71,7 +87,7 @@ delicon.setAttribute("class","ri-delete-bin-line")
 addItem.setAttribute("class","add-items")
 addicon.setAttribute('class',"ri-add-line")
 
-// append card element
+
 cards.appendChild(card)
 card.appendChild(h2Text)
 card.appendChild(hrline)
@@ -83,86 +99,137 @@ operation.appendChild(addItem)
 addItem.appendChild(addicon)
 cardTitle.value = ''
 noitems.innerHTML= ''
+
+
+findcrationpopup.style.backgroundColor = 'rgb(8, 210, 62)'
+   findcrationpopuph1.innerHTML= 'Success - Card Created Successfully'
+
+      setTimeout(() => {
+      findcrationpopuph1.innerHTML= ''
+
+      findcrationpopup.removeAttribute("id")
+       
+      }, 1000);
 // delete card 
 deleteTask.addEventListener('click', function(){
+   console.log("deleted")
    card.remove()
    blurPage[1].style.display="none"
-singleCardTrip.innerText= cardTitle.value
 
  closeTask()
+ findcrationpopup.setAttribute("id","creationpopup")
+
+ findcrationpopup.style.backgroundColor = 'rgb(255, 91, 123)'
+   findcrationpopuph1.innerHTML= 'Card Deleted Successfully'
+
+      setTimeout(() => {
+      findcrationpopuph1.innerHTML= ''
+
+      findcrationpopup.removeAttribute("id")
+       
+      }, 1000);
 })
+
+
+addItem.addEventListener('click', function(){
+   let add_new_items_in_card =document.createElement("div")
+   let h2AddIt = document.createElement("h2")
+   let inputIt = document.createElement("input")
+   let add_close_button_in_card = document.createElement("div")
+   let add_in_card = document.createElement("div")
+   let close_in_card = document.createElement("div")
+   
+   
+   
+   // set class id attribute inside popup to show item inside card
+   add_new_items_in_card.setAttribute("class","add_new_items_in_card")
+   h2AddIt.innerText ="Add New Item"
+   inputIt.setAttribute("id","carditems")
+   inputIt.setAttribute("type","text")
+   add_close_button_in_card.setAttribute("class", "add_close_button_in_card")
+   add_in_card.setAttribute("class", "add_in_card")
+   add_in_card.setAttribute("style", "cursor: pointer")
+   add_in_card.innerText="Add"
+   close_in_card.setAttribute("class", "close_in_card")
+   close_in_card.setAttribute("style", "cursor: pointer")
+   close_in_card.innerText="Close"
+     
+   
+   
+   container.appendChild(add_new_items_in_card)
+   add_new_items_in_card.appendChild(h2AddIt)
+   add_new_items_in_card.appendChild(inputIt)
+   add_new_items_in_card.appendChild(add_close_button_in_card)
+   add_close_button_in_card.appendChild(add_in_card)
+   add_close_button_in_card.appendChild(close_in_card)
+   blurPage[0].style.display="block"
+   
+   // onclick add crate mark item inside card
+   add_in_card.addEventListener('click',function(){
+      if(inputIt.value==''){
+         findcrationpopup.setAttribute("id","creationpopup")
+
+          findcrationpopup.style.backgroundColor = 'rgb(210, 149, 8)'
+        findcrationpopuph1.innerHTML= 'please enter some text'
+
+      setTimeout(() => {
+      findcrationpopuph1.innerHTML= ''
+
+      findcrationpopup.removeAttribute("id")
+       
+      }, 1000);
+      }
+      else{
+         let item = document.createElement("div")
+      item.setAttribute("class","item")
+      cardItems.appendChild(item)
+      let h3Text = document.createElement("h3")
+      let mark = document.createElement("div")
+      mark.setAttribute("class","mark")
+   mark.innerText = "mark done"
+   item.appendChild(h3Text)
+   item.appendChild(mark)
+      h3Text.innerText=inputIt.value
+   
+   // onclick mark done  mark as read
+      mark.addEventListener('click',function(){
+         h3Text.style.textDecoration="line-through"
+         h3Text.style.color="orangered"
+         item.removeChild(mark)
+         item.setAttribute("style", "display:flex;align-items:center; justify-content:center;")
+   
+      })
+   
+      inputIt.value =''
+
+      findcrationpopup.setAttribute("id","creationpopup")
+
+      findcrationpopup.style.backgroundColor = 'rgb(15, 190, 9) '
+    findcrationpopuph1.innerHTML= 'Task Created Successfully'
+
+  setTimeout(() => {
+  findcrationpopuph1.innerHTML= ''
+
+  findcrationpopup.removeAttribute("id")
+   
+  }, 1000);
+      }
+
+      
+   })
+   
+   // onclick close inside card item add popup
+   close_in_card.addEventListener('click', function(){
+      container.removeChild(add_new_items_in_card) 
+   blurPage[0].style.display="none"
+      
+         })
+   
+   })
+
 
 
 //add items inside card to show popup
-addItem.addEventListener('click', function(){
-let add_new_items_in_card =document.createElement("div")
-let h2AddIt = document.createElement("h2")
-let inputIt = document.createElement("input")
-let add_close_button_in_card = document.createElement("div")
-let add_in_card = document.createElement("div")
-let close_in_card = document.createElement("div")
-
-
-
-// set class id attribute inside popup to show item inside card
-add_new_items_in_card.setAttribute("class","add_new_items_in_card")
-h2AddIt.innerText ="Add New Item"
-inputIt.setAttribute("id","carditems")
-inputIt.setAttribute("type","text")
-add_close_button_in_card.setAttribute("class", "add_close_button_in_card")
-add_in_card.setAttribute("class", "add_in_card")
-add_in_card.setAttribute("style", "cursor: pointer")
-add_in_card.innerText="Add"
-close_in_card.setAttribute("class", "close_in_card")
-close_in_card.setAttribute("style", "cursor: pointer")
-close_in_card.innerText="Close"
-  
-
-
-container.appendChild(add_new_items_in_card)
-add_new_items_in_card.appendChild(h2AddIt)
-add_new_items_in_card.appendChild(inputIt)
-add_new_items_in_card.appendChild(add_close_button_in_card)
-add_close_button_in_card.appendChild(add_in_card)
-add_close_button_in_card.appendChild(close_in_card)
-blurPage[0].style.display="block"
-
-// onclick add crate mark item inside card
-add_in_card.addEventListener('click',function(){
-   let item = document.createElement("div")
-   item.setAttribute("class","item")
-   cardItems.appendChild(item)
-   let h3Text = document.createElement("h3")
-   let mark = document.createElement("div")
-   mark.setAttribute("class","mark")
-mark.innerText = "mark done"
-item.appendChild(h3Text)
-item.appendChild(mark)
-   h3Text.innerText=inputIt.value
-
-// onclick mark done  mark as read
-   mark.addEventListener('click',function(){
-      h3Text.style.textDecoration="line-through"
-      h3Text.style.color="orangered"
-      item.removeChild(mark)
-      item.setAttribute("style", "display:flex;align-items:center; justify-content:center;")
-
-   })
-
-   inputIt.value =''
-})
-
-// onclick close inside card item add popup
-close_in_card.addEventListener('click', function(){
-   container.removeChild(add_new_items_in_card) 
-blurPage[0].style.display="none"
-   
-      })
-
-})
-
-
-
 let gobacknav = document.createElement("div")
   let backbutton = document.createElement("div")
   let backicon = document.createElement("i")
@@ -236,6 +303,16 @@ backbutton.addEventListener('click',function(){
    container.removeChild(add_new_items_in_card)
 })
 })
+
+
+
+
+   }
+   
+// append card element
+
+
+
 
 }
 
